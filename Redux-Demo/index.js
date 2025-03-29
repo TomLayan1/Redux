a6624bea24965071fb24c7d98e6ba18b44468f2b
TomLayan1 <afolayantomisin1919@gmail.com>
2025-03-29 14:30:28 +0100

    Added a reducer to update the state of the application based on the action

--- Redux-Demo/index.js
+++ Redux-Demo/index.js
@@ -1,3 +1,9 @@
+// State of the app. Should be an object
+const initialState = {
+  numOfCakes: 10
+}
+
+// Name of the action
 const buy_cake = 'buy_cake';
 
 // Create an action
@@ -6,5 +12,20 @@ function buyCake() {
   return {
     type: buy_cake,
     info: 'First redux action'
+  }
+}
+
+// Create a reducer
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case buy_cake: return {
+      // Return a copy of the state object. Use the spread operator
+      ...state,
+      // Update only the property that need to be updated
+      numOfCakes: state.numOfCakes - 1
+    }
+
+    // If there's an action we've not accounted for return the state as it is
+    default: return state 
   }
 }
