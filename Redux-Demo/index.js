e7c81faf92b751b5e98950bc57e584d9e6438f96
TomLayan1 <afolayantomisin1919@gmail.com>
2025-03-29 13:56:10 +0100

    Created an action

--- Redux-Demo/index.js
+++ Redux-Demo/index.js
@@ -1,1 +1,10 @@
-console.log('Hello from index');
+const buy_cake = 'buy_cake';
+
+// Create an action
+// buyCake function is an action creator that returns an action
+function buyCake() {
+  return {
+    type: buy_cake,
+    info: 'First redux action'
+  }
+}
