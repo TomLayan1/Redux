7fd11fa426129dbd137b931f683fe8ed8d41effd
TomLayan1 <afolayantomisin1919@gmail.com>
2025-03-30 15:05:58 +0100

    Redux async action setup

---
+++ Redux-Demo/asyncActioni.js
@@ -0,0 +1,60 @@
+const redux = require('redux');
+const createStore = redux.createStore
+
+// Define the state
+const initialState = {
+  loading: false,
+  users: [],
+  error: ''
+}
+
+// Actions
+const FETCH_USER_REQUEST = 'FETCH_USER_REQUEST'
+const FETCH_USER_SUCCESS = 'FETCH_USER_SUCCESS'
+const FETCH_USER_FAILURE = 'FETCH_USER_FAILURE'
+
+// Action creators
+const fetchUserRequest = () => {
+  return {
+    type: FETCH_USER_REQUEST
+  }
+}
+
+const fetchUserSuccess = users => {
+  return {
+    type: FETCH_USER_SUCCESS,
+    payload: users
+  }
+}
+
+const fetchUserFailure = error => {
+  return {
+    type: FETCH_USER_FAILURE,
+    payload: error
+  }
+}
+
+// Reducer
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_USER_REQUEST:
+      return {
+        ...state,
+        loading: true
+      }
+    case FETCH_USER_SUCCESS: 
+      return {
+        loading: false,
+        users: action.payload,
+        error: ''
+      }
+    case FETCH_USER_FAILURE:
+      return {
+        loading: false,
+        users: [],
+        error: action.payload
+      }
+  }
+}
+
+const store = createStore(reducer)
